6440a43c27de921284537a09e57ab0dd18fee1f2
tbauz <tbauzil@gmail.com>
2022-06-07 12:46:34 +0200

    Compte example sur la page d'acceuil

--- PinderV2/src/app/home/home.page.ts
+++ PinderV2/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import axios from 'axios';
 
 
 @Component({
@@ -8,7 +9,28 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  
-  constructor() {}
+  default : any = [];
+  data : any;
+
+  constructor() {
+    axios.get('http://localhost:3000/profil_default')
+    .then(resp => {
+        this.data = resp.data;
+        this.data.forEach(p => {
+            const profil = {
+              "id" : p.id,
+              "pseudo": p.pseudo,
+              "dateNaissance": p.dateNaissance,
+              "photo": p.photo,
+            }
+          this.default.push(profil);
+        });
+        console.log(this.default);
+        
+    })
+    .catch(error => {
+        console.log(error);
+    });
+  }
   
 }
