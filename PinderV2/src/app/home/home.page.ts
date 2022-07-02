ed762fbf5d5e8eaaa39c2cc85124427262be1eac
Hugo <hugo.peyron@etu.uca.fr>
2022-07-02 17:33:11 +0200

    utilisation d'un servide déployé sur angular pour le db.json

--- PinderV2/src/app/home/home.page.ts
+++ PinderV2/src/app/home/home.page.ts
@@ -22,7 +22,7 @@ export class HomePage {
    }
 
   constructor(private storage: Storage, private toastCtrl: ToastController,public navCtrl: NavController) {
-    axios.get('http://localhost:3000/profil_default')
+    axios.get('https://angular-json-db.herokuapp.com/profil_default')
     .then(resp => {
         this.data = resp.data;
         this.data.forEach(p => {
